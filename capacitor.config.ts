906d6c08a1792568ace3d782b860b93423c1b2d0
Peter-98 <pma751@inlumine.ual.es>
2023-06-05 10:56:54 +0200

    ambios android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,9 +2,14 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'fireapp',
+  appName: 'tunelyApp',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  plugins: {
+    android: {
+      minSdkVersion: 24
+    }
+  }
 };
 
 export default config;
