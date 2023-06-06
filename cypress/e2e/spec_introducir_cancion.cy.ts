7c2e24120f662aa8c61c1260ff083b435172fd5f
Peter-98 <pma751@inlumine.ual.es>
2023-06-06 13:48:40 +0200

    tests

--- cypress/e2e/spec_introducir_cancion.cy.ts
+++ cypress/e2e/spec_introducir_cancion.cy.ts
@@ -17,11 +17,23 @@ describe('Inicio de sesión', () => {
     cy.contains('ion-title', 'All songs');
   });
 
-  it('inicio de sesión exitoso', () => {
+  it('inserta cancion existosamente', () => {
     cy.get('.search-container > .button-has-icon-only').click();
 
-
     cy.get('.ion-color-success').click();
+
+     // Rellenar el formulario de carga de canciones
+     cy.get('ion-input[formControlName="name"]').type('Canción de prueba');
+     cy.get('ion-input[formControlName="artist"]').type('Artista de prueba');
+     cy.get('ion-input[formControlName="album"]').type('Álbum de prueba');
+     cy.get('ion-input[formControlName="releaseDate"]').type('2022-01-01');
+     cy.get('ion-input[formControlName="duration"]').type('240');
+     cy.get('ion-input[formControlName="uri"]').type('spotify:track:0123456789');
+     cy.get('ion-input[formControlName="image"]').type('https://www.soy502.com/sites/default/files/styles/mobile_full_node/public/2017/Nov/20/por_que_repites_la_misma_cancion.jpg');
+ 
+     // Hacer clic en el botón "Save Song"
+     cy.get('.div-button-create > .md').click();
+ 
 
   });
   
