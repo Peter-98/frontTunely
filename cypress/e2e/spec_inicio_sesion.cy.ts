adb9bb3a0996e029b89c37408c629b68a8b91de6
Peter-98 <pma751@inlumine.ual.es>
2023-06-06 13:25:42 +0200

    tests

--- cypress/e2e/spec_inicio_sesion.cy.ts
+++ cypress/e2e/spec_inicio_sesion.cy.ts
@@ -4,19 +4,60 @@ describe('Inicio de sesión', () => {
     cy.visit('http://pedromrtaguilera.tech');
   });
 
-  it('debería permitir el inicio de sesión exitoso', () => {
+  it('inicio de sesión exitoso', () => {
     // Hacer clic en el botón "Log in"
     cy.contains('Log in').click();
 
         // Rellenar el formulario de inicio de sesión
-        cy.get('ion-input[formControlName="email"]').type('tu_email@example.com');
-        cy.get('ion-input[formControlName="password"]').type('tu_contraseña');
+        cy.get('ion-input[formControlName="email"]').type('pedro@hola.es');
+        cy.get('ion-input[formControlName="password"]').type('sdasdadasd');
         
         // Enviar el formulario
         cy.get('ion-button[type="submit"]').click();
         
         // Verificar que se haya iniciado sesión exitosamente
-        cy.contains('h1', 'Life is music');
-        cy.contains('button', 'All songs');
-      });
+        cy.contains('ion-title', 'All songs');
+  });
+
+  it('inicio de sesion incorrecto', () => {
+    // Hacer clic en el botón "Log in"
+    cy.contains('Log in').click();
+  
+    // Rellenar el formulario de inicio de sesión con credenciales incorrectas
+    cy.get('ion-input[formControlName="email"]').type('usuario_incorrecto@example.com');
+    cy.get('ion-input[formControlName="password"]').type('contraseña_incorrecta');
+  
+    // Enviar el formulario
+    cy.get('ion-button[type="submit"]').click();
+  
+    // Verificar que se muestre un mensaje de error de inicio de sesión
+    cy.contains('.error-message', 'Error en la autenticación de MongoDB');
+  });
+
+  it('intentar iniciar sesión con un email incorrecto', () => {
+    // Hacer clic en el botón "Log in"
+    cy.contains('Log in').click();
+  
+    // Rellenar el formulario de inicio de sesión con un email incorrecto
+    cy.get('ion-input[formControlName="email"]').type('email_incorrecto');
+    cy.get('ion-input[formControlName="password"]').type('contraseña_valida');
+  
+    // Verificar que se muestre un mensaje de error de email incorrecto
+    cy.contains('.error-message', 'The email format is not correct.');
+  });
+  
+  it('intentar iniciar sesión con una contraseña demasiado corta', () => {
+    // Hacer clic en el botón "Log in"
+    cy.contains('Log in').click();
+  
+    // Rellenar el formulario de inicio de sesión con una contraseña demasiado corta
+    cy.get('ion-input[formControlName="email"]').type('usuario_valido@example.com');
+    cy.get('ion-input[formControlName="password"]').type('123');
+
+  
+    // Verificar que se muestre un mensaje de error de contraseña demasiado corta
+    cy.contains('.error-message', 'The minimum length of a password is 6 characters.');
+  });
+  
+  
 });
